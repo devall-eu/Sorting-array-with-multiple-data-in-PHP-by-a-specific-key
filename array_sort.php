7c5ed4b9501a7e36821f3898516efe0728e14b7f
Mario <devall.eu@gmail.com>
2019-08-14 21:42:55 +0200

    Added function

---
+++ array_sort.php
@@ -0,0 +1,48 @@
+<?php
+
+$array = array();
+
+array_push($array, array('name' => 'John', 'age' => 23));
+array_push($array, array('name' => 'Peter', 'age' => 31));
+
+$array = array_sort($array, 'age', SORT_DESC);
+
+echo '<pre>';
+print_r($array);
+echo '</pre>';
+
+
+function array_sort($array, $on, $order = SORT_ASC)
+{
+    $new_array = array();
+    $sortable_array = array();
+
+    if (count($array) > 0) {
+        foreach ($array as $k => $v) {
+            if (is_array($v)) {
+                foreach ($v as $k2 => $v2) {
+                    if ($k2 == $on) {
+                        $sortable_array[$k] = $v2;
+                    }
+                }
+            } else {
+                $sortable_array[$k] = $v;
+            }
+        }
+
+        switch ($order) {
+            case SORT_ASC:
+                asort($sortable_array);
+                break;
+            case SORT_DESC:
+                arsort($sortable_array);
+                break;
+        }
+
+        foreach ($sortable_array as $k => $v) {
+            $new_array[$k] = $array[$k];
+        }
+    }
+
+    return $new_array;
+}
